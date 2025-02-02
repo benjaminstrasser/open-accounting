482a2ab166c1f0b1738f1e5efbec67e1bcd51fd8
Benjamin Strasser <benjamin.strasser@aots.io>
2025-02-02 18:56:53 +0100

    added sidebar
    Signed-off-by: Benjamin Strasser <benjamin.strasser@aots.io>

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Book from "lucide-svelte/icons/book";
+	import House from "lucide-svelte/icons/house";
+	import NotebookPen from "lucide-svelte/icons/notebook-pen";
+	import * as Sidebar from "$lib/components/ui/sidebar";
+
+	// Menu items.
+	const items = [
+		{
+			title: "Home",
+			url: "/",
+			icon: House,
+		},
+		{
+			title: "Journal",
+			url: "/journal",
+			icon: NotebookPen
+		},
+		{
+			title: "Accounts",
+			url: "/account",
+			icon: Book,
+		}
+	];
+</script>
+
+<Sidebar.Root>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
--- src/lib/components/ui/sidebar/sidebar-provider.svelte
+++ src/lib/components/ui/sidebar/sidebar-provider.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
-	import { cn } from '$lib/utils.js';
-	import type { WithElementRef } from 'bits-ui';
-	import type { HTMLAttributes } from 'svelte/elements';
+	import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+	import { cn } from "$lib/utils.js";
+	import type { WithElementRef } from "bits-ui";
+	import type { HTMLAttributes } from "svelte/elements";
 	import {
 		SIDEBAR_COOKIE_MAX_AGE,
 		SIDEBAR_COOKIE_NAME,
 		SIDEBAR_WIDTH,
-		SIDEBAR_WIDTH_ICON
-	} from './constants.js';
-	import { setSidebar } from './context.svelte.js';
+		SIDEBAR_WIDTH_ICON,
+	} from "./constants.js";
+	import { setSidebar } from "./context.svelte.js";
 
 	let {
 		ref = $bindable(null),
 		open = $bindable(true),
-		onOpenChange = () => {
-		},
+		onOpenChange = () => {},
 		class: className,
 		style,
 		children,
@@ -33,7 +32,7 @@
 
 			// This sets the cookie to keep the sidebar state.
 			document.cookie = `${SIDEBAR_COOKIE_NAME}=${open}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
-		}
+		},
 	});
 </script>
 
