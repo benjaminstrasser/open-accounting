33be2b640946d615fd816936f2feb9748a1ce813
Benjamin Strasser <benjamin.strasser@aots.io>
2025-02-17 13:56:09 +0100

    added pdf-viewer in its own component
    Signed-off-by: Benjamin Strasser <benjamin.strasser@aots.io>

---
+++ src/lib/components/pdf-viewer.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import * as pdfjs from 'pdfjs-dist';
+	// eslint-disable-next-line
+	import * as worker from 'pdfjs-dist/build/pdf.worker.mjs';
+
+	// BASE64 encoded PDF data
+	let { file_data }: { file_data: string } = $props();
+
+	let pdfproxy: pdfjs.PDFDocumentProxy = $state(undefined);
+
+	pdfjs.GlobalWorkerOptions.workerSrc = `pdfjs-dist/build/pdf.worker.mjs`; // Using DocumentInitParameters object to load binary data.
+
+	$effect(() => {
+		console.log('loading');
+		var loadingTask = pdfjs.getDocument({ data: atob(file_data) });
+		console.log(loadingTask);
+		loadingTask.promise.then(
+			function (pdf) {
+				pdfproxy = pdf;
+				console.log('PDF loaded'); // Fetch the first page
+				for (let i = 1; pdf.numPages >= i; i++) {
+					renderPage(i);
+				}
+			},
+			function (reason) {
+				// PDF loading error
+				console.error(reason);
+			}
+		);
+	});
+
+	function renderPage(pageNumber: number) {
+		pdfproxy.getPage(pageNumber).then(function (page) {
+			console.log('Page loaded');
+
+			var scale = 2;
+			var viewport = page.getViewport({ scale: scale }); // Prepare canvas using PDF page dimensions
+
+			var canvas = document.getElementById(`canvas-${pageNumber}`) as HTMLCanvasElement;
+			var context = canvas.getContext('2d');
+			canvas.height = viewport.height;
+			canvas.width = viewport.width; // Render PDF page into canvas context
+
+			var renderContext = {
+				canvasContext: context,
+				viewport: viewport
+			};
+			var renderTask = page.render(renderContext);
+			renderTask.promise.then(function () {
+				console.log('Page rendered');
+			});
+		});
+	}
+</script>
+
+{#if pdfproxy}
+	<div class="flex flex-col gap-3">
+		{#each Array.from({ length: pdfproxy.numPages }, (_, i) => i + 1) as page}
+			<canvas id={`canvas-${page}`}></canvas>
+		{/each}
+	</div>
+{/if}
--- src/routes/draft-invoice/[draftid]/+page.svelte
+++ src/routes/draft-invoice/[draftid]/+page.svelte
@@ -1,64 +1,10 @@
 <script lang="ts">
-	import * as pdfjs from 'pdfjs-dist';
-	// eslint-disable-next-line
-	import * as worker from 'pdfjs-dist/build/pdf.worker.mjs';
 	import type { PageProps } from './$types';
+	import PdfViewer from '$lib/components/pdf-viewer.svelte';
 
 	const { data }: PageProps = $props();
-
-	let pdfproxy: pdfjs.PDFDocumentProxy = $state(undefined);
-
-	pdfjs.GlobalWorkerOptions.workerSrc = `pdfjs-dist/build/pdf.worker.mjs`; // Using DocumentInitParameters object to load binary data.
-
-	$effect(() => {
-		console.log('loading');
-		var loadingTask = pdfjs.getDocument({ data: atob(data.draftInvoice.file_data) });
-		console.log(loadingTask);
-		loadingTask.promise.then(
-			function (pdf) {
-				pdfproxy = pdf;
-				console.log('PDF loaded'); // Fetch the first page
-				for (let i = 1; pdf.numPages >= i; i++) {
-					renderPage(i);
-				}
-			},
-			function (reason) {
-				// PDF loading error
-				console.error(reason);
-			}
-		);
-	});
-
-	function renderPage(pageNumber: number) {
-		pdfproxy.getPage(pageNumber).then(function (page) {
-			console.log('Page loaded');
-
-			var scale = 2;
-			var viewport = page.getViewport({ scale: scale }); // Prepare canvas using PDF page dimensions
-
-			var canvas = document.getElementById(`canvas-${pageNumber}`) as HTMLCanvasElement;
-			var context = canvas.getContext('2d');
-			canvas.height = viewport.height;
-			canvas.width = viewport.width; // Render PDF page into canvas context
-
-			var renderContext = {
-				canvasContext: context,
-				viewport: viewport
-			};
-			var renderTask = page.render(renderContext);
-			renderTask.promise.then(function () {
-				console.log('Page rendered');
-			});
-		});
-	}
 </script>
 
 <div class="max-h-screen w-1/2 overflow-y-scroll">
-	{#if pdfproxy}
-		<div class="flex flex-col gap-3">
-			{#each Array.from({ length: pdfproxy.numPages }, (_, i) => i + 1) as page}
-				<canvas id={`canvas-${page}`}></canvas>
-			{/each}
-		</div>
-	{/if}
+	<PdfViewer file_data={data.draftInvoice.file_data} />
 </div>
