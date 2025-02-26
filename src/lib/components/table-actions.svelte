235c58c392b7359566468dca60392c7e28a156d5
Martin Ledl <martin.ledl@aots.io>
2025-02-26 17:37:39 +0100

    adds simple table actions dropdown

---
+++ src/lib/components/table-actions.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" module>
+	export type TableAction = {
+		label: string;
+		handler: () => void;
+	};
+</script>
+
+<script lang="ts">
+	import Ellipsis from 'lucide-svelte/icons/ellipsis';
+	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+
+	type Props = {
+		actions: TableAction[];
+	};
+
+	const { actions }: Props = $props();
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		{#snippet child({ props })}
+			<Button {...props} variant="ghost" size="icon" class="relative size-8 p-0">
+				<span class="sr-only">Open menu</span>
+				<Ellipsis />
+			</Button>
+		{/snippet}
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		{#each actions as action}
+			<DropdownMenu.Item onclick={action.handler}>{action.label}</DropdownMenu.Item>
+		{/each}
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
